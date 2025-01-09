6889b60eae70cd12d93d9ba8bf1e275750d779fc
Ikennaf1 <iiikbros202@gmail.com>
2025-01-09 22:22:18 +0100

    slides ok

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./src/*.{html,css}",
+    "./*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
