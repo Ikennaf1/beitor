6d962fe5b5292cee6f5a61018d5dcac9dac3a72f
Ikennaf1 <iiikbros202@gmail.com>
2025-01-29 21:26:24 +0100

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 var navBtn = document.querySelector('#nav-open');
 var menu = document.querySelector('#nav-opened')
 navBtn.onclick = () => {
-    menu.hidden = !menu.hidden;
-    navBtn.ariaHidden = !navBtn.ariaHidden;
-    navBtn.ariaExpanded = !navBtn.ariaExpanded;
+  menu.hidden = !menu.hidden;
+  navBtn.ariaHidden = !navBtn.ariaHidden;
+  navBtn.ariaExpanded = !navBtn.ariaExpanded;
 }
