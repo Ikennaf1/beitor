3d3f77b0ff9ba781807300fc076352ea4ec005f1
Ikennaf1 <iiikbros202@gmail.com>
2025-02-05 23:36:46 +0100

    Hope

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-var navBtn = document.querySelector('#nav-open');
-var menu = document.querySelector('#nav-opened')
-navBtn.onclick = () => {
-  menu.hidden = !menu.hidden;
-  navBtn.ariaHidden = !navBtn.ariaHidden;
-  navBtn.ariaExpanded = !navBtn.ariaExpanded;
-}
